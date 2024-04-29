7aa07c5598287827381af25b20e311a0bc63994a
Sadra <sadra.shameli1@gmail.com>
2024-04-29 13:13:51 +0200

    fixed a small bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,18 @@ import ProfilePicture from '~/assets/images/me.jpg';
 
 export default function HomePage() {
     return (
-        <main className='mx-auto grid max-w-main space-y-16 px-6 md:h-screen md:grid-rows-3 md:space-y-0 xl:px-0'>
+        <main className='mx-auto grid max-w-main space-y-16 px-6 lg:h-screen xl:grid-rows-3 xl:space-y-0 xl:px-0'>
             <div>
                 <Navbar />
             </div>
 
             <div className='flex flex-col-reverse justify-between xl:flex-row'>
                 <div className='my-auto mt-10 grid justify-center space-y-3 xl:mt-auto'>
-                    <h1 className='text-center text-4xl font-semibold text-white lg:text-7xl'>{Resume.basics.title}</h1>
-                    <span className='bg-gradient-indigo text-center text-xl font-semibold xl:text-start max-w-fit'>{Resume.basics.role}</span>
+                    <h1 className='text-center text-4xl font-semibold text-white tracking-tight lg:text-6xl xl:text-7xl'>{Resume.basics.title}</h1>
+                    <span className='bg-gradient-indigo mx-auto max-w-fit text-center text-xl font-semibold xl:mx-0 xl:text-start tracking-tight'>{Resume.basics.role}</span>
                 </div>
 
-                <Image className='self-center rounded-2xl sm:max-w-lg xl:mx-0 xl:self-auto' src={ProfilePicture} alt='Profile picture' priority />
+                <Image className='self-center rounded-2xl object-cover sm:max-w-lg xl:mx-0 xl:self-auto' src={ProfilePicture} alt='Profile picture' priority />
             </div>
 
             <div className='self-end pb-10'>
