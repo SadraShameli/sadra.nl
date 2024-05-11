dcced5dec7f541f0ee7a6bacbcde37a144131b80
Sadra <sadra.shameli1@gmail.com>
2024-05-11 18:41:37 +0200

    Maybe this time

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
-const config = { trailingSlash: false, skipTrailingSlashRedirect: true };
+const config = {};
 
 export default config;
