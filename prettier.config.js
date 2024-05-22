99f4768396b6e2e94e3ef80b9c1da0bafeadb731
Sadra Shameli <sadra.shameli1@gmail.com>
2024-05-22 10:59:43 +0200

    Added reading section

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,6 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
+    endOfLine: 'lf',
     singleQuote: true,
     tabWidth: 4,
     plugins: ['prettier-plugin-tailwindcss'],
