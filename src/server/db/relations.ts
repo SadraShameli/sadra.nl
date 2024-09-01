b9d57314c815fc85870f014ef699cd27a2b5fd20
Sadra <sadra.shameli1@gmail.com>
2024-09-01 09:19:25 +0200

    chore: code optimizations seed

--- src/server/db/relations.ts
+++ src/server/db/relations.ts
@@ -2,61 +2,63 @@ import { relations } from 'drizzle-orm';
 
 import { device, location, reading, recording, sensor, sensorsToDevices } from './schema';
 
-export const locationRelations = relations(location, ({ many }) => ({
-    devices: many(device),
-    readings: many(reading),
-    recordings: many(recording),
-}));
+export const databaseRelations = {
+    location: relations(location, ({ many }) => ({
+        devices: many(device),
+        readings: many(reading),
+        recordings: many(recording),
+    })),
 
-export const sensorRelations = relations(sensor, ({ many }) => ({
-    sensorsToDevices: many(sensorsToDevices),
-    readings: many(reading),
-    recordings: many(recording),
-}));
+    sensor: relations(sensor, ({ many }) => ({
+        sensorsToDevices: many(sensorsToDevices),
+        readings: many(reading),
+        recordings: many(recording),
+    })),
 
-export const deviceRelations = relations(device, ({ one, many }) => ({
-    location: one(location, {
-        fields: [device.location_id],
-        references: [location.id],
-    }),
-    sensorsToDevices: many(sensorsToDevices),
-    readings: many(reading),
-    recordings: many(recording),
-}));
+    device: relations(device, ({ one, many }) => ({
+        location: one(location, {
+            fields: [device.location_id],
+            references: [location.id],
+        }),
+        sensorsToDevices: many(sensorsToDevices),
+        readings: many(reading),
+        recordings: many(recording),
+    })),
 
-export const readingRelations = relations(reading, ({ one }) => ({
-    device: one(device, {
-        fields: [reading.device_id],
-        references: [device.id],
-    }),
-    sensor: one(sensor, {
-        fields: [reading.sensor_id],
-        references: [sensor.id],
-    }),
-    location: one(location, {
-        fields: [reading.location_id],
-        references: [location.id],
-    }),
-}));
+    reading: relations(reading, ({ one }) => ({
+        device: one(device, {
+            fields: [reading.device_id],
+            references: [device.id],
+        }),
+        sensor: one(sensor, {
+            fields: [reading.sensor_id],
+            references: [sensor.id],
+        }),
+        location: one(location, {
+            fields: [reading.location_id],
+            references: [location.id],
+        }),
+    })),
 
-export const recordingRelations = relations(recording, ({ one }) => ({
-    device: one(device, {
-        fields: [recording.device_id],
-        references: [device.id],
-    }),
-    location: one(location, {
-        fields: [recording.location_id],
-        references: [location.id],
-    }),
-}));
+    recording: relations(recording, ({ one }) => ({
+        device: one(device, {
+            fields: [recording.device_id],
+            references: [device.id],
+        }),
+        location: one(location, {
+            fields: [recording.location_id],
+            references: [location.id],
+        }),
+    })),
 
-export const sensorToDeviceRelations = relations(sensorsToDevices, ({ one }) => ({
-    sensor: one(sensor, {
-        fields: [sensorsToDevices.sensor_id],
-        references: [sensor.id],
-    }),
-    device: one(device, {
-        fields: [sensorsToDevices.device_id],
-        references: [device.id],
-    }),
-}));
+    sensorsToDevices: relations(sensorsToDevices, ({ one }) => ({
+        sensor: one(sensor, {
+            fields: [sensorsToDevices.sensor_id],
+            references: [sensor.id],
+        }),
+        device: one(device, {
+            fields: [sensorsToDevices.device_id],
+            references: [device.id],
+        }),
+    })),
+};
--- src/server/db/seed.ts
+++ src/server/db/seed.ts
@@ -1,21 +1,10 @@
 import { endDb } from '.';
-
-import DeviceSeed from './seeds/device';
-import LocationSeed from './seeds/location';
-import ReadingSeed from './seeds/reading';
-import RecordingSeed from './seeds/recording';
-import SensorSeed from './seeds/sensor';
-import SensorsToDevicesSeed from './seeds/sensorsToDevices';
+import { DatabaseSeeder } from './types';
 
 async function main() {
     console.log('Seeding database');
 
-    await LocationSeed();
-    await SensorSeed();
-    await DeviceSeed();
-    await ReadingSeed();
-    await RecordingSeed();
-    await SensorsToDevicesSeed();
+    await DatabaseSeeder.runAll();
 }
 
 main()
